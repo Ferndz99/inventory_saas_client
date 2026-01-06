4f944a96e5a71399d52d05a26a047ddb8042b6a6
Sebastian <fernandezbarrera.sebastian@gmail.com>
2026-01-06 14:35:23 -0300

    feat: add responsive problem section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/layout/Navbar";
+import ProblemSection from "@/components/ProblemSection";
 
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <ProblemSection />
     </div>
   );
 }
