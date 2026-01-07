fe120459cf9280c6628afd5723bde6d07a7add79
Sebastian <fernandezbarrera.sebastian@gmail.com>
2026-01-07 13:24:53 -0300

    feat: add onboarding layout and onboarding company create page

---
+++ schemas/onboarding.schema.ts
@@ -0,0 +1,15 @@
+// app/onboarding/company/schema.ts
+import { z } from 'zod'
+
+export const createCompanySchema = z.object({
+    company_name: z
+        .string()
+        .min(2, 'El nombre de la empresa es muy corto'),
+    company_rut: z
+        .string()
+        .min(8, 'Ingresa un RUT válido'),
+})
+
+export type CreateCompanyFormValues = z.infer<
+    typeof createCompanySchema
+>
